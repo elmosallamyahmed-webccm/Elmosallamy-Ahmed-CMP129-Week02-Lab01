06dd3d07c0665f38644547e86ad9668d56d9b828
elmosallamyahmed-webccm <elmosallamy.ahmed@student.ccm.edu>
2026-09-08 18:07:38 -0400

    Done with part 1 and 2

--- CelsiusTable.java
+++ CelsiusTable.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
 public class CelsiusTable {
-    int hi;
-    
+    public static void main(String[] args) {
+        Scanner wow = new Scanner(System.in);
+        double [] tempsF = new double[20];
+        double [] tempsC = new double[20];
+        for(int i = 0; i<20; i++){
+            System.out.printf("What is the degree in Farenheit for temperature %d%n", i+1);
+            tempsF[i] = wow.nextDouble();
+            tempsC[i] =  celcius(tempsF[i]);
+        }
+        for(int i= 0; i<20; i++){
+            System.out.printf("The temperature %.2f F is equivalent to %.2f C%n", tempsF[i], tempsC[i]);
+        }
+    }
+    public static double celcius(double F){
+        double C = (F-32)*(5.0/9);
+        return C;
+    }   
 }
--- KineticEnergy.java
+++ KineticEnergy.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 public class KineticEnergy{
- public static void main(String[] args){
-   Scanner hi = new Scanner(System.in);
-   System.out.println("What is your mass in kg?");
-   double mass = hi.nextDouble();
-   System.out.println("What is your velocity in meters per second?");
-   double velocity = hi.nextDouble();
-   double KE = calculation(mass,velocity);
-   System.out.printf("The kinetic energy is %d", KE);
+    public static void main(String[] args){
+    Scanner hi = new Scanner(System.in);
+    System.out.println("What is your mass in kg?");
+    double mass = hi.nextDouble();
+    System.out.println("What is your velocity in meters per second?");
+    double velocity = hi.nextDouble();
+    double KE = kineticEnergy(mass,velocity);
+    System.out.printf("The kinetic energy is %.2f", KE);
 }
-public int KineticEnergy()
+public static double kineticEnergy(double mass, double velocity){
+    double KE = 0.5 * mass * (velocity*velocity);
+    return KE;
+}}
 

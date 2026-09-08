7582579fc955fb104d10b92779d68f863b4d74df
elmosallamyahmed-webccm <elmosallamy.ahmed@student.ccm.edu>
2026-09-08 16:59:11 -0400

    Half of Kinetic Lab Done

--- CelsiusTable.java
+++ CelsiusTable.java
@@ -1,3 +1,4 @@
 public class CelsiusTable {
+    int hi;
     
 }
--- KineticEnergy.java
+++ KineticEnergy.java
@@ -1,3 +1,13 @@
-public class KineticEnergy {
-
+import java.util.Scanner;
+public class KineticEnergy{
+ public static void main(String[] args){
+   Scanner hi = new Scanner(System.in);
+   System.out.println("What is your mass in kg?");
+   double mass = hi.nextDouble();
+   System.out.println("What is your velocity in meters per second?");
+   double velocity = hi.nextDouble();
+   double KE = calculation(mass,velocity);
+   System.out.printf("The kinetic energy is %d", KE);
 }
+public int KineticEnergy()
+

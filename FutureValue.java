ca24ae797ad968fe3edaa9bf1d06b546574957f5
elmosallamyahmed-webccm <elmosallamy.ahmed@student.ccm.edu>
2026-09-09 16:12:11 -0400

    Using the KE file to backbone the Future Value to make things easier

--- FutureValue.java
+++ FutureValue.java
@@ -1,4 +1,18 @@
-public class FutureValue {
-    
-    
+import java.util.Scanner;
+public class FutureValue{
+    public static void main(String[] args){
+    Scanner hi = new Scanner(System.in);
+    System.out.println("What is your current value?");
+    double P = hi.nextDouble();
+    System.out.println("What is your monthly interest rate in % form?");
+    double i = hi.nextDouble()/100;
+    System.out.println("How long will it stay in the account for");
+
+    System.out.printf("The kinetic energy is %.2f", KE);
 }
+public static double kineticEnergy(double mass, double velocity){
+    double KE = 0.5 * mass * (velocity*velocity);
+    return KE;
+}}
+
+
